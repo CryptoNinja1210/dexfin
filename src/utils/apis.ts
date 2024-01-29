d55fcdaa1e11aca852ce62beb5dd37a8973164fd
CryptoNinja1210 <crypto.ninja1210@gmail.com>
2024-01-30 01:21:52 +0800

    stockchat

--- src/utils/apis.ts
+++ src/utils/apis.ts
@@ -5,7 +5,8 @@ export const SingleCoin = (id: any) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
 
 export const HistoricalChart = (id: any, days = 365, currency: any) =>
-  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
+  `https://api.coingecko.com/api/v3/coins/${id}/ohlc?vs_currency=${currency}&days=${days}&precision=1`;
 
 export const TrendingCoins = (currency: any) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
+  // https://api.coingecko.com/api/v3/coins/ethereum/ohlc?vs_currency=usd&days=30&precision=1
